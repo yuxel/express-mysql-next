fdfba56fa0d6283321eacdd414d3db7e07250c91
Osman Yuksel <yuxel@sonsuzdongu.com>
2023-04-08 00:34:53 +0300

    Express version updated, jade dependency removed

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const handle = nextApp.getRequestHandler();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
